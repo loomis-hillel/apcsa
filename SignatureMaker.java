910f45dca6381cd686a8eb7b28cb89d6a90cf136
loomis-hillel <89052159+loomis-hillel@users.noreply.github.com>
2022-09-08 06:04:50 -0400

    Create SignatureMaker.java

---
+++ SignatureMaker.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+    A sample program that utilizes String manipulation to
+    convert user input to a signature.
+    @author <your name>
+    @version <the date>
+*/
+public class SignatureMaker {
+
+    public static void main(String[] args) {
+        
+        /* We still haven't talked about this object yet. */
+        Scanner keyboard = new Scanner ( System.in );
+        /* This collects the user input so we can use it as a String. */
+        System.out.print ( "Enter your full name: " );
+        String fullName = keyboard.nextLine( );
+
+        /* Part I: Count Characters */
+
+
+        /* Part II: Build a Initials */
+
+
+        /* Part III: Build a Signature */
+
+
+
+    }
+}
