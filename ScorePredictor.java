2ecc937861998e367c4014521bc704141a7944fc
loomis-hillel <89052159+loomis-hillel@users.noreply.github.com>
2022-09-14 23:04:48 -0400

    Create ScorePredictor.java

---
+++ ScorePredictor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Calculates the average of the expected final scores for
+ * the Scheck Hillel football team in the 2022 season.
+ * @author <Your name>
+ * @version <Version>
+ */
+public class ScorePredictor {
+
+	public static void main(String[] args) {
+
+		// You code goes here.
+
+	}
+
+}
