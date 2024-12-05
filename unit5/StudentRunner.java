c35b1691fa407b5ed2d62da43c4255a74c798d0b
loomis-hillel <89052159+loomis-hillel@users.noreply.github.com>
2024-12-05 12:02:38 -0500

    Update StudentRunner.java

--- unit5/StudentRunner.java
+++ unit5/StudentRunner.java
@@ -1,6 +1,6 @@
 public class StudentRunner {
     public static void main ( String [] args) {
-        Student ale = new Student ( "Ale", "123", 4.5 );
-        System.out.println ( ale );
+        Student theYitz = new Student ( "Yitzhak", "123", 4.5 );
+        System.out.println ( theYitz );
     }
 }
